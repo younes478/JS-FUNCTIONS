cb355fb172098cb6f054bc5238a8480f553b3988
younes <younes.fiotmane@fullcircl.com>
2024-07-09 14:55:22 +0100

    new py file

--- python.py
+++ python.py
@@ -1,13 +1,19 @@
-# List comprehension
-squares = [x**2 for x in range(10)]
+# # List comprehension
+# squares = [x**2 for x in range(10)]
 
-# Function with default argument
-def greet(name="World"):
-    return f"Hello, {name}!"
+# # Function with default argument
+# def greet(name="World"):
+#     return f"Hello, {name}!"
 
-# Dictionary
-person = {"name": "Alice", "age": 30}
+# # Dictionary
+# person = {"name": "Alice", "age": 30}
 
-# Loop
-for i in range(5):
-    print(i)
+# # Loop
+# for i in range(19):
+#     print(i)
+
+
+def read (name, age):
+    print('Hi my name is',name,', and I am',age,'years old')
+
+read('jo',35)
