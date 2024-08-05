0c9f85dd3af623ccf8186eb2b15e5e7c9c1f1d60
younes <younes.fiotmane@fullcircl.com>
2024-08-05 16:43:57 +0100

    new class

---
+++ houseClass.js
@@ -0,0 +1,15 @@
+class house {
+    constructor (size, price) {
+        this.size = size
+        this.price = price
+
+    }
+ pr(price, size) {
+    console.log(`price is ${price}, and size is ${size}`) 
+    
+}
+
+}
+
+let house01 = new house (40, 90000)
+house01.pr(9000,40)
